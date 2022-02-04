56e579bc14de73fe95ac6469399f87b6197c3111
nkg-laufire <nkg@laufire.in>
2022-02-04 13:25:49 +0530

    Included dev config.

--- index.js
+++ index.js
@@ -2,20 +2,32 @@ const ejs = require('ejs');
 const fs = require('fs');
 const config = require('./config');
 
-const { collection: { map } } = require('@laufire/utils');
+const { collection: { map }} = require('@laufire/utils');
 const { App } = config;
 
-const generate = (inputFile, data, outputFile) => {
-	ejs.renderFile(`${inputFile}.ejs`, data, (err, output) => {
-		fs.writeFile(`dist/${outputFile}.js`, output, (err) => {
-			if (err) throw err;
-		});
-	});
+const generate = (
+	inputFile, data, outputFile
+) => {
+	ejs.renderFile(
+		`${ inputFile }.ejs`, data, (err, output) => {
+			fs.writeFile(
+				`dist/${ outputFile }.js`, output, (fsErr) => {
+					if(fsErr)
+						throw fsErr;
+				}
+			);
+		}
+	);
 };
 
 map(App, (component) => {
 	const { name } = component;
-	generate('button', component, name);
+
+	generate(
+		'button', component, name
+	);
 });
 
-generate('app', { ...config, map }, 'app');
+generate(
+	'app', { App, map }, 'app'
+);
