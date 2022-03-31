3ecf1116d38ea8d7d92c78fac5954180f22b94fb
nkg-laufire <nkg@laufire.in>
2022-03-31 14:14:09 +0530

    Refactored.

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+/* eslint-disable no-await-in-loop */
 const {
 	read, processTemplate, buildContext, normalizeContent,
 	buildContent,
 } = require('./src/lib/repoManager');
 const saveCode = require('./src/lib/saveCode');
 
+const asyncReduce = async (collection, acc) => {
+	while(collection.length)
+		// eslint-disable-next-line no-param-reassign
+		acc = await collection.shift()(acc);
+
+	return acc;
+};
+
 const render = async (initial) => {
 	const context = await read({
 		...await buildContext(initial),
 		localPath: 'dist/source',
 	});
-	const normalized = normalizeContent(context);
-	const built = buildContent(normalized);
+	const methods = [normalizeContent, buildContent, processTemplate, saveCode];
 
-	const processed = await processTemplate(built);
-
-	await saveCode(processed);
+	await asyncReduce(methods, context);
 };
 
 render({});
