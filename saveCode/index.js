0d795827e43186a0b5fc6f346016bbb5a08a4aa6
nkg-laufire <nkg@laufire.in>
2022-04-08 16:03:03 +0530

    Restructured saveCode.

--- saveCode/index.js
+++ saveCode/index.js
@@ -1,8 +1,13 @@
 const writeCode = require('./writeCode');
 const commitCode = require('./commitCode');
+const prepareBase = require('../src/lib/prepareBase');
 
 const saveCode = async (context) => {
-	writeCode(context);
+	const { config: { template }} = context;
+	const { source } = require(`../templates/${ template }/config`);
+
+	await prepareBase({ ...context, source });
+	await writeCode(context);
 	await commitCode(context);
 };
 
