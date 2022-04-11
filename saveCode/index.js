ae3d7152cd5bd0330af36f2ffd07c13e9a281c5f
nkg-laufire <nkg@laufire.in>
2022-04-11 14:27:27 +0530

    Abstracted config.source.

--- saveCode/index.js
+++ saveCode/index.js
@@ -3,10 +3,7 @@ const commitCode = require('./commitCode');
 const prepareBase = require('../src/lib/prepareBase');
 
 const saveCode = async (context) => {
-	const { config: { template }} = context;
-	const { source } = require(`../templates/${ template }/config`);
-
-	await prepareBase({ ...context, source });
+	await prepareBase(context);
 	await writeCode(context);
 	await commitCode(context);
 };
